5e9c885e12d31a3983de6becabbd01f1e7ed7ac3
vaistau <vaistaugaityte@gmail.com>
2021-11-29 16:41:13 -0600

    update index.html and script.js

---
+++ script.js
@@ -0,0 +1,90 @@
+var playAgain = 'yes';
+
+let computerScore = 0
+let humanScore = 0
+
+for (i = 0; i < 5 && (playAgain === 'yes' || playAgain === 'yes, of course!'); i++) {
+    const buttons = document.querySelectorAll('button');
+    buttons.forEach((button) => {
+        button.addEventListener('click', () => {
+            let playerSelection = event.target.id;
+            let computerSelection = computerSelection();
+            playRound(playerSelection, computerSelection);
+        })
+    })
+
+    let playerSelection = prompt('I want to play a game... Will you choose rock, paper, or scissors?').toLowerCase();
+    let randomNumber = Math.floor(Math.random() * (1 + 2 - 0));
+
+    function computerSelection() {
+        if (randomNumber === 0) {
+            return 'rock';
+        } else if (randomNumber === 1) {
+            return 'paper';
+        } else if (randomNumber === 2) {
+            return 'scissors';
+        } else {
+            return 'Something went wrong...';
+        }
+    }
+
+    function playRound() {
+        if (playerSelection !== 'rock' && playerSelection !== 'paper' && playerSelection !== 'scissors') {
+            i--
+            return '...Am I a joke to you?';
+        }
+
+        if (playerSelection === computerSelection()) {
+            i--
+            return 'Ties don\'t count.'
+        }
+
+        if (playerSelection === 'rock') {
+            if (computerSelection() === 'paper') {
+                computerScore++;
+                return 'Computer wins! Paper beats rock.';
+            }
+            if (computerSelection() === 'scissors') {
+                humanScore++;
+                return 'Human wins... Rock beats scissors.';
+            }
+        } else if (playerSelection === 'paper') {
+            if (computerSelection() === 'scissors') {
+                computerScore++;
+                return 'Computer wins! Scissors beat paper.';
+            }
+            if (computerSelection() === 'rock') {
+                humanScore++;
+                return 'Human wins... Paper beats rock.';
+            }
+        } else if (playerSelection === 'scissors') {
+            if (computerSelection() === 'rock') {
+                computerScore++;
+                return 'Computer wins! Rock beats scissors.';
+            }
+            if (computerSelection() === 'paper') {
+                humanScore++;
+                return 'Human wins... Scissors beat paper.';
+            }
+        }
+    }
+
+    console.log(playRound())
+    console.log('Human: ' + humanScore + ', Computer: ' + computerScore);
+
+    /*if (i !== 4) {
+        playAgain = prompt('Would you like to play again?', 'Yes, of course!').toLowerCase();
+    }
+
+    if (playAgain !== 'yes' && playAgain !== 'yes, of course!') {
+        console.log('Fine then. Be that way.');
+    }*/
+}
+
+if (playAgain === 'yes' || playAgain === 'yes, of course!') {
+    if (humanScore > computerScore) {
+        console.log('Human won. Don\'t let it get to your head.');
+    } else if (humanScore < computerScore) {
+        console.log('Computer won. As it should be.');
+    }
+}
