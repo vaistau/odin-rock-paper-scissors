a1605275f52d70b82567b2e68dc398e8376079f3
vaistau <vaistaugaityte@gmail.com>
2021-11-29 17:01:28 -0600

    update index.html and script.js

--- script.js
+++ script.js
@@ -3,17 +3,8 @@ var playAgain = 'yes';
 let computerScore = 0
 let humanScore = 0
 
-for (i = 0; i < 5 && (playAgain === 'yes' || playAgain === 'yes, of course!'); i++) {
-    const buttons = document.querySelectorAll('button');
-    buttons.forEach((button) => {
-        button.addEventListener('click', () => {
-            let playerSelection = event.target.id;
-            let computerSelection = computerSelection();
-            playRound(playerSelection, computerSelection);
-        })
-    })
-
-    let playerSelection = prompt('I want to play a game... Will you choose rock, paper, or scissors?').toLowerCase();
+for (i = 0; i < 5 /*&& (playAgain === 'yes' || playAgain === 'yes, of course!')*/; i++) {
+    let playerSelection = prompt('I want to play a game on your browser console... Will you choose rock, paper, or scissors?').toLowerCase();
     let randomNumber = Math.floor(Math.random() * (1 + 2 - 0));
 
     function computerSelection() {
@@ -72,7 +63,7 @@ for (i = 0; i < 5 && (playAgain === 'yes' || playAgain === 'yes, of course!'); i
     console.log(playRound())
     console.log('Human: ' + humanScore + ', Computer: ' + computerScore);
 
-    /*if (i !== 4) {
+   /* if (i !== 4) {
         playAgain = prompt('Would you like to play again?', 'Yes, of course!').toLowerCase();
     }
 
@@ -81,10 +72,10 @@ for (i = 0; i < 5 && (playAgain === 'yes' || playAgain === 'yes, of course!'); i
     }*/
 }
 
-if (playAgain === 'yes' || playAgain === 'yes, of course!') {
+/*if (playAgain === 'yes' || playAgain === 'yes, of course!') {
     if (humanScore > computerScore) {
         console.log('Human won. Don\'t let it get to your head.');
     } else if (humanScore < computerScore) {
         console.log('Computer won. As it should be.');
     }
-}
+}*/
